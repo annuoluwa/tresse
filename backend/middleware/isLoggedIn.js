14b38002856f123406454563010e9026b22ce7f1
Elizabeth <zeereigns@hotmail.com>
2025-11-09 08:17:18 +0000

    refactor: isolate isLoggedIn and isAdmin middleware

---
+++ backend/middleware/isLoggedIn.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+      return next();
+  }
+  return res.status(401).json({ message: "Not authenticated" });
+};
+
+module.exports= isLoggedIn;
